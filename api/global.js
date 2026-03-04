a24e2e75cb260dfdc17e87c998929fca7392d909
mucmib <mucmib@gmail.com>
2026-03-04 17:49:21 +0100

    Add files via upload

--- api/global.js
+++ api/global.js
@@ -3,10 +3,10 @@ import { kv } from "@vercel/kv";
 
 function todayKey() {
   const d = new Date();
-  const y = d.getUTCFullYear();
-  const m = String(d.getUTCMonth() + 1).padStart(2, "0");
-  const day = String(d.getUTCDate()).padStart(2, "0");
-  return `${y}${m}${day}`; // UTC-Tag
+  const y = d.getFullYear();
+  const m = String(d.getMonth() + 1).padStart(2, "0");
+  const day = String(d.getDate()).padStart(2, "0");
+  return `${y}${m}${day}`;
 }
 
 export default async function handler(req, res) {
