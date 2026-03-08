c05993c2a3fce8d21a1f25366e93530243ffd30b
mucmib <mucmib@gmail.com>
2026-03-08 07:57:45 +0100

    Add files via upload

--- api/impact.js
+++ api/impact.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
 
   res.status(200).json({
     letters_total: 22420,
-    meta_views: 1605106,
+    meta_views: 1710731,
     youtube_views: 827279,
     tiktok_views: 166000,
     last_update: "2026-03-03T10:30:00.000Z"
